df87c39b0ef28e75ea0142ba5042b80def601ae1
Korshukov Stepan <korshukovstepan@gmail.com>
2023-05-09 14:24:19 +0300

    feat: add search page

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
 /* eslint-env browser */
 import createSwiper from './modules/trainersSlider';
 import openMenu from './modules/openMenu';
-import makeSearch from './modules/makeSearch';
+// import makeSearch from './modules/makeSearch';
 import changeLang from './modules/changeLang';
 import smoothScroll from './modules/smoothScroll';
 
 window.addEventListener('DOMContentLoaded', () => {
-  makeSearch(
-    '.options__search-icon',
-    '.search-bar',
-    'search-bar--active',
-    '.header',
-    '.header__bottom',
-  );
+  // makeSearch(
+  //   '.options__search-icon',
+  //   '.search-bar',
+  //   'search-bar--active',
+  //   '.header',
+  //   '.header__bottom',
+  // );
   openMenu();
   changeLang();
   createSwiper();
